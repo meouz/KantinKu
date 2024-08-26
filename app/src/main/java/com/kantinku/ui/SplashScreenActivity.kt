ba9aa16eeee082eafeaef82cf97c0347e14d650d
meouz <samuel.deanova@gmail.com>
2024-08-26 10:04:38 +0700

    fix: transparent status bar

--- app/src/main/java/com/kantinku/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/kantinku/ui/SplashScreenActivity.kt
@@ -1,31 +1,46 @@
 package com.kantinku.ui
 
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.kantinku.R
 import com.kantinku.ui.auth.LoginActivity
+import com.kantinku.ui.forgotpass.ForgotPassActivateActivity
 
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(
-            WindowManager.LayoutParams.ANIMATION_CHANGED,
-            WindowManager.LayoutParams.ANIMATION_CHANGED);
-        supportActionBar?.hide()
+        // Hide the status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
         
         setContentView(R.layout.activity_splash_screen)
         
         Handler().postDelayed({
-            window.setWindowAnimations(android.R.style.Animation_Toast)
             startActivity(Intent(this, LoginActivity::class.java))
+            
+            // give transition effect
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }, 2500)
+    }
+    
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
     }
 }
